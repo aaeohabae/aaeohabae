7780d7cbb5c79fa5c05634fe37296e3eadfe1401
aaeohabae <154266460+aaeohabae@users.noreply.github.com>
2023-12-20 05:23:23 +0545

    Update config.py

--- config.py
+++ config.py
@@ -11,11 +11,11 @@ load_dotenv()
 # Mandatory variables for the bot to start
 API_ID = int(os.environ.get("API_ID", '18076374')) #API ID from https://my.telegram.org/auth
 API_HASH = os.environ.get("API_HASH", '17d273ec3475c2c171d9122fd079774c') #API Hash from https://my.telegram.org/auth
-BOT_TOKEN = os.environ.get("BOT_TOKEN", '6055950045:AAFTLM6rpLbHvI-g9xa4tIAECoqy3fPqAhw') # Bot token from @BotFather
+BOT_TOKEN = os.environ.get("BOT_TOKEN", '') # Bot token from @BotFather
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split(",")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", 'SHARE')
-DATABASE_URL = os.environ.get("DATABASE_URL", 'mongodb+srv://SK:SK@cluster0.hbkrl.mongodb.net/?retryWrites=true&w=majority') # mongodb uri from https://www.mongodb.com/
+DATABASE_URL = os.environ.get("DATABASE_URL", 'mongodb+srv://aaroha:aaroha@cluster0.b73vc2y.mongodb.net/?retryWrites=true&w=majority') # mongodb uri from https://www.mongodb.com/
 OWNER_ID =  int(os.environ.get("OWNER_ID", '5111685964')) # id of the owner
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(5111685964)
